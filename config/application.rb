9a79fa161a296feae13b27ad94c85adf632673fd
Uli Kimmich <uli@globalvirtualstudio.com>
2014-01-10 14:15:08 -1000

    Commit

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,8 @@ module GesundJetzt
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+
   end
 end
